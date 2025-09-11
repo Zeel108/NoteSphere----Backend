bad950fdc849ecb00a1f7431f72f71fa3919ccf6
zeel <sundayy119@gmail.com>
2025-09-11 18:22:47 +0530

    created spring starter project and add tomcat dependanciy

---
+++ src/main/java/com/Zeel/NoteSphereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Zeel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteSphereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteSphereBackendApplication.class, args);
+	}
+
+}
